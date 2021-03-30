8d1893e42f95b48766523f6781b9a276c2954278
Ian Edward Hein Prieto <ianhein50@gmail.com>
2021-03-30 14:33:05 -0300

    Improved view

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
     has_many :books
+
+    validates :name, :presence => true, :uniqueness => true
+    validates :abreviation, length: {minimum: 3}, allow_blank: true
+
+    #validates :abreviation, length
 end
