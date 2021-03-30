b8e95342a3289e2c0037fae130f3bc69212a28a1
Ian Edward Hein Prieto <ianhein50@gmail.com>
2021-03-30 15:00:01 -0300

    asd

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
   # GET /books or /books.json
   def index
     @books = Book.all
-    @books = @books.where(["title LIKE :filter", :filter => ("%" + params[:filter] + "%")]) unless params [:filter].blank?
+    #@books = @books.where(["title LIKE :filter", :filter => ("%" + params[:filter] + "%")]) unless params [:filter].blank?
+    @books = @books.where(title: "asd")
   end
 
   # GET /books/1 or /books/1.json
