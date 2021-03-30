822fd262e4d5cd8e5428d3a55c7be2cccd7bc716
Ian Edward Hein Prieto <ianhein50@gmail.com>
2021-03-30 14:35:08 -0300

    bug fixed

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
     has_many :books
 
     validates :name, :presence => true, :uniqueness => true
-    validates :abreviation, length: {minimum: 3}, allow_blank: true
+    validates :abbreviation, length: {minimum: 3}, allow_blank: true
 
     #validates :abreviation, length
 end
